6de200cde980c9500092440ca37e8949b88537a2
wmaneesh <36091416+wmaneesh@users.noreply.github.com>
2019-06-29 18:28:49 -0400

    Delete testing--package-view-spec.js

--- testing--package/spec/testing--package-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import Testing-ackageView from '../lib/testing--package-view';
-
-describe('Testing-ackageView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
